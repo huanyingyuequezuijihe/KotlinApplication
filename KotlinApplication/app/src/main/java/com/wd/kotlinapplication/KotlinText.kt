6454522333c042be91afdbd5f4222a904e51c1b4
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-11 14:37:55 +0800

    Kotlin基础用法-变量

--- KotlinApplication/app/src/main/java/com/wd/kotlinapplication/KotlinText.kt
+++ KotlinApplication/app/src/main/java/com/wd/kotlinapplication/KotlinText.kt
@@ -11,4 +11,11 @@ package com.wd.kotlinapplication
 //main函数  程序入口  是计算机默认找的的一个运行的函数
 fun main(args:Array<String>){
     println("hello world")
+    //变量的声明及使用
+    //第一次创建好空间后，是什么类型就固定了，不可更改
+    var name="后羿";
+    println(name)
+    //再次赋值  会覆盖
+    name="嫦娥"
+    println(name)
 }
