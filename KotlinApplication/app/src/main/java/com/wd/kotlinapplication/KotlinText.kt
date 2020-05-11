3c96ca2738081a2a51add8058abc2efeff289196
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-11 14:27:07 +0800

    Kotlin基础用法-项目创建完成-hello world

---
+++ KotlinApplication/app/src/main/java/com/wd/kotlinapplication/KotlinText.kt
@@ -0,0 +1,14 @@
+package com.wd.kotlinapplication
+
+/**
+ * @author 王阳
+ * Class :1708A
+ * @description:
+ * @date :2020/5/11 14:16
+ * @classname :KotlinText
+ */
+
+//main函数  程序入口  是计算机默认找的的一个运行的函数
+fun main(args:Array<String>){
+    println("hello world")
+}
