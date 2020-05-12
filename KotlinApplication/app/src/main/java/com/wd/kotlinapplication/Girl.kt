04dd792a0da45bd1de61a396971868fc4c78c58d
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-12 10:15:20 +0800

    Kotlin基础用法-静态属性，动态行为

---
+++ KotlinApplication/app/src/main/java/com/wd/kotlinapplication/Girl.kt
@@ -0,0 +1,27 @@
+package com.wd.kotlinapplication
+
+/**
+ * @author 王阳
+ * Class :1708A
+ * @description:
+ * @date :2020/5/12 10:03
+ * @classname :Girl
+ */
+class Girl(var chacyor:String,var voice:String){
+    fun smile(){
+        println("妹子笑了一下，好甜呀")
+    }
+    fun cry(){
+        println("呜呜呜，人家好伤心")
+    }
+}
+
+fun main(arg:Array<String>) {
+    val girl1 = Girl("御姐", "空灵")
+    println("girl1这位妹子的长相:${girl1.chacyor},声音:${girl1.voice}")
+
+    //静态属性  动态行为
+    //静态指他的属性，动态指他的方法
+    girl1.smile()
+    girl1.cry()
+}
