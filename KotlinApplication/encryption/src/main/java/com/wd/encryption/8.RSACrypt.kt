ddd9f186bc86d1e67ce5a7f82e228aeaf45d911a
huanyingyuequezuijihe <1165413404@qq.com>
2020-05-13 15:24:19 +0800

    Kotlin基础-RSA公钥，私钥加密

--- KotlinApplication/encryption/src/main/java/com/wd/encryption/8.RSACrypt.kt
+++ KotlinApplication/encryption/src/main/java/com/wd/encryption/8.RSACrypt.kt
@@ -2,6 +2,8 @@ package com.wd.encryption
 
 import java.security.Key
 import java.security.KeyPairGenerator
+import java.security.PrivateKey
+import java.security.PublicKey
 import javax.crypto.Cipher
 
 /**
@@ -11,6 +13,40 @@ import javax.crypto.Cipher
  * @date :2020/5/13 14:51
  * @classname :RSACrypt
  */
+object RSACrypt{
+    //
+    val transformation="RSA"
+
+    /**私钥加密
+     * input  原文
+     * privateKey  私钥
+     */
+    fun encryptBiPrivateKey(input:String,privateKey:PrivateKey): String? {
+        //1.创建cipher
+         val cipher = Cipher.getInstance(transformation)
+         //2.初始化cipher
+         //val key: Key?=null
+         cipher.init(Cipher.ENCRYPT_MODE,privateKey)
+        //3.加密/解密
+        val encrypt = cipher.doFinal(input.toByteArray())
+        return Base64.encode(encrypt)
+    }
+
+    /**公钥加密
+     * input  原文
+     * privateKey  私钥
+     */
+    fun encryptBiPublicKey(input:String,publicKey: PublicKey): String? {
+        //1.创建cipher
+        val cipher = Cipher.getInstance(transformation)
+        //2.初始化cipher
+        //val key: Key?=null
+        cipher.init(Cipher.ENCRYPT_MODE,publicKey)
+        //3.加密/解密
+        val encrypt = cipher.doFinal(input.toByteArray())
+        return Base64.encode(encrypt)
+    }
+}
 fun main(args:Array<String>){
     //如何生成密钥对
     //密钥对生成器
@@ -29,4 +65,12 @@ fun main(args:Array<String>){
     val key: Key?=null
     cipher.init(Cipher.ENCRYPT_MODE,key)*/
     //3.加密/解密
+
+    val input="幻影月缺醉几何"
+    //调用加密
+    val encrypt = RSACrypt.encryptBiPrivateKey(input, privateKey)
+    println("RSA私钥加密="+encrypt)
+
+    val encryption = RSACrypt.encryptBiPublicKey(input, publicKey)
+    println("RSA公钥加密="+encryption)
 }
